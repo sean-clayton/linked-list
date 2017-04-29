7fd41bf56710335b9259128bf78e0026c37464eb
Sean Clayton <sean@seanclayton.me>
2017-04-29 15:40:29 -0400

    Make tests correct for enumerable implementation

--- lib/enumerable.ex
+++ lib/enumerable.ex
@@ -1,11 +1,13 @@
 defimpl Enumerable, for: LinkedList do
-  def count(%LinkedList{value: nil, next: nil}), do: {:ok, 0}
-
-  def count(node), do: {:ok, LinkedList.length(node)}
+  def count(node) do
+    {:ok, LinkedList.length(node)}
+  end
 
   def member?(node, value) do
     {:ok, LinkedList.some(node, fn x -> x == value end)}
   end
 
-  def reduce(node, initial_value, fun), do: {:ok, LinkedList.reduce(node, initial_value, fun)}
+  def reduce(node, initial_value, fun) do
+    {:ok, LinkedList.reduce(node, initial_value, fun)}
+  end
 end
--- spec/linked_list_spec.exs
+++ spec/linked_list_spec.exs
@@ -424,57 +424,57 @@ defmodule LinkedListSpec do
   describe "Enumerable count implementation" do
     it "returns a count of an empty linked list" do
       expect(LinkedList.create() |> Enum.count)
-      |> to(eq {:ok, 0})
+      |> to(eq 0)
     end
 
     it "returns a count of a single-node linked list" do
       expect(LinkedList.create(1) |> Enum.count)
-      |> to(eq {:ok, 1})
+      |> to(eq 1)
     end
 
     it "returns a count of a large linked list" do
       expect(LinkedList.create(1, LinkedList.create(2, LinkedList.create(3))) |> Enum.count)
-      |> to(eq {:ok, 3})
+      |> to(eq 3)
     end
   end
 
   describe "Enumerable member? implementation" do
     it "returns false if given an empty linked list" do
       expect(LinkedList.create() |> Enum.member?(1))
-      |> to(eq {:ok, false})
+      |> to(eq false)
     end
 
     it "determines if a node exists in a single-node linked list" do
       expect(LinkedList.create(1) |> Enum.member?(1))
-      |> to(eq {:ok, true})
+      |> to(eq true)
 
       expect(LinkedList.create(1) |> Enum.member?(0))
-      |> to(eq {:ok, false})
+      |> to(eq false)
     end
 
     it "determines if a node exists in a large linked list" do
       expect(LinkedList.create(1, LinkedList.create(2, LinkedList.create(3))) |> Enum.member?(1))
-      |> to(eq {:ok, true})
+      |> to(eq true)
 
       expect(LinkedList.create(1, LinkedList.create(2, LinkedList.create(3))) |> Enum.member?(0))
-      |> to(eq {:ok, false})
+      |> to(eq false)
     end
   end
 
   describe "Enumerable reduce implementation" do
     it "reduces an empty linked list" do
       expect(LinkedList.create() |> Enum.reduce(0, fn curr, acc -> curr + acc end))
-      |> to(eq {:ok, 0})
+      |> to(eq 0)
     end
 
     it "reduces a single-node linked list" do
       expect(LinkedList.create(1) |> Enum.reduce(0, fn curr, acc -> curr + acc end))
-      |> to(eq {:ok, 1})
+      |> to(eq 1)
     end
 
     it "reduces a large linked list" do
       expect(LinkedList.create(1, LinkedList.create(2, LinkedList.create(3))) |> Enum.reduce(0, fn curr, acc -> curr + acc end))
-      |> to(eq {:ok, 6})
+      |> to(eq 6)
     end
   end
 end
