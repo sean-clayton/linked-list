36abb87f135e506161969beb66a4f19ecabf62bd
Sean Clayton <sean@seanclayton.me>
2017-04-29 11:41:56 -0400

    Add new test framework

--- mix.exs
+++ mix.exs
@@ -2,14 +2,22 @@ defmodule LinkedList.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :linked_list,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]]
+    [
+      app: :linked_list,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        espec: :test,
+      ]
+    ]
   end
 
   # Configuration for the OTP application
@@ -35,6 +43,7 @@ defmodule LinkedList.Mixfile do
       {:credo, "~> 0.7", only: [:dev, :test]},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:excoveralls, "~> 0.6", only: :test},
+      {:espec, "~> 1.3.4", only: [:dev, :test]},
     ]
   end
 end
---
+++ spec/linked_list_spec.exs
@@ -0,0 +1,417 @@
+defmodule LinkedListSpec do
+  use ESpec, async: true
+  doctest LinkedList
+
+  context "create" do
+    it "creates an empty node" do
+      expect LinkedList.create() |> to(eq :empty)
+    end
+
+    it "creates a single node" do
+      expect LinkedList.create(1) |> to(eq %LinkedList{
+        value: 1,
+        next: :empty
+      })
+    end
+
+    it "creates multiple nodes" do
+      expect(
+        LinkedList.create(1, LinkedList.create(2, LinkedList.create(3)))
+      ) |> to(eq %LinkedList{
+        value: 1,
+        next: %LinkedList{
+          value: 2,
+          next: %LinkedList{
+            value: 3,
+            next: :empty
+          }
+        }
+      })
+    end
+  end
+
+  context "empty?" do
+    it "can tell if an empty node is empty" do
+      expect LinkedList.empty?(:empty)
+      |> to(eq true)
+    end
+
+    it "can tell if a non-empty node is" do
+      expect LinkedList.empty?(LinkedList.create(1))
+      |> to(eq false)
+    end
+  end
+
+  context "from_list" do
+    it "creates an empty linked list from an empty list" do
+      expect([] |> LinkedList.from_list)
+      |> to(eq LinkedList.create())
+    end
+
+    it "creates a single-node linked-list from a single-value list" do
+      expect([1] |> LinkedList.from_list)
+      |> to(eq LinkedList.create(1))
+    end
+
+    it "creates a large linked-list from a large list" do
+      expect([1, 2, 3] |> LinkedList.from_list)
+      |> to(eq LinkedList.create(1, LinkedList.create(2, LinkedList.create(3))))
+    end
+  end
+
+  context "to_list" do
+    it "creates an empty list from an empty linked list" do
+      expect([] |> LinkedList.from_list |> LinkedList.to_list)
+      |> to(eq [])
+    end
+
+    it "creates a single-value list from a single-node linked list" do
+      expect([1] |> LinkedList.from_list |> LinkedList.to_list)
+      |> to(eq [1])
+    end
+
+    it "creates a large list from a large linked-list" do
+      expect([1, 2, 3] |> LinkedList.from_list |> LinkedList.to_list)
+      |> to(eq [1, 2, 3])
+    end
+  end
+
+  context "length" do
+    it "returns 0 from an empty linked-list" do
+      expect([] |> LinkedList.from_list |> LinkedList.length)
+      |> to(eq 0)
+    end
+
+    it "returns 1 from a single-node linked-list" do
+      expect([1] |> LinkedList.from_list |> LinkedList.length)
+      |> to(eq 1)
+    end
+
+    it "returns a large number from a large linked-list" do
+      expect([1, 2, 3] |> LinkedList.from_list |> LinkedList.length)
+      |> to(eq 3)
+    end
+  end
+
+  context "map" do
+    it "returns an empty linked list if given an empty linked list" do
+      expect(
+        []
+        |> LinkedList.from_list
+        |> LinkedList.map(fn x -> x * 2 end)
+        |> LinkedList.to_list
+      ) |> to(eq [])
+    end
+
+    it "maps a single-node linked list" do
+      expect(
+        [1]
+        |> LinkedList.from_list
+        |> LinkedList.map(fn x -> x * 2 end)
+        |> LinkedList.to_list
+      ) |> to(eq [2])
+    end
+
+    it "maps a large linked list" do
+      expect(
+        [1, 2, 3]
+        |> LinkedList.from_list
+        |> LinkedList.map(fn x -> x * 2 end)
+        |> LinkedList.to_list
+      ) |> to(eq [2, 4, 6])
+    end
+  end
+
+  context "filter" do
+    it "returns an empty linked list if given an empty linked list" do
+      expect(
+        []
+        |> LinkedList.from_list
+        |> LinkedList.filter(fn x -> x < 5 end)
+        |> LinkedList.to_list
+      ) |> to(eq [])
+    end
+
+    it "filters a single-node linked list" do
+      expect(
+        [1]
+        |> LinkedList.from_list
+        |> LinkedList.filter(fn x -> x < 5 end)
+        |> LinkedList.to_list
+      ) |> to(eq [1])
+    end
+
+    it "filters a large linked list" do
+      expect(
+        [1, 2, 2, 4, 3, 6, 4, 8]
+        |> LinkedList.from_list
+        |> LinkedList.filter(fn x -> x < 5 end)
+        |> LinkedList.to_list
+      ) |> to(eq [1, 2, 2, 4, 3, 4])
+    end
+  end
+
+  context "reduce" do
+    it "returns an empty linked list if given an empty linked list" do
+      expect(
+        []
+        |> LinkedList.from_list
+        |> LinkedList.reduce(0, fn curr, acc -> curr + acc end)
+      ) |> to(eq 0)
+    end
+
+    it "reduces a single-node linked list" do
+      expect(
+        [1]
+        |> LinkedList.from_list
+        |> LinkedList.reduce(0, fn curr, acc -> curr + acc end)
+      ) |> to(eq 1)
+    end
+
+    it "reduces a large linked list" do
+      expect(
+        [1, 2, 3, 4, 5]
+        |> LinkedList.from_list
+        |> LinkedList.reduce(0, fn curr, acc -> curr + acc end)
+      ) |> to(eq 15)
+    end
+  end
+
+  context "join" do
+    it "returns an empty linked list if given an empty linked list" do
+      expect(
+        []
+        |> LinkedList.from_list
+        |> LinkedList.join(" ")
+      ) |> to(eq "")
+    end
+
+    it "joins single-node linked list" do
+      expect(
+        [1]
+        |> LinkedList.from_list
+        |> LinkedList.join(" ")
+      ) |> to(eq "1")
+    end
+
+    it "joins a large linked list with no delimeter provided" do
+      expect(
+        [1, 2, 3]
+        |> LinkedList.from_list
+        |> LinkedList.join
+      ) |> to(eq "123")
+    end
+
+    it "joins a large linked list" do
+      expect(
+        [1, 2, 3]
+        |> LinkedList.from_list
+        |> LinkedList.join(" ")
+      ) |> to(eq "1 2 3")
+    end
+  end
+
+  context "to_string" do
+    it "turns an empty linked list to a string" do
+      expect(
+        []
+        |> LinkedList.from_list
+        |> LinkedList.to_string
+      ) |> to(eq "[]")
+    end
+
+    it "turns a single-node linked list to a string" do
+      expect(
+        [1]
+        |> LinkedList.from_list
+        |> LinkedList.to_string
+      ) |> to(eq "[1]")
+    end
+
+    it "turns a large linked list to a string" do
+      expect(
+        [1, 2, 3]
+        |> LinkedList.from_list
+        |> LinkedList.to_string
+      ) |> to(eq "[1, 2, 3]")
+    end
+  end
+
+  context "concat" do
+    it "returns an empty linked list if given empty linked lists" do
+      expect(
+        LinkedList.concat(:empty, :empty)
+        |> LinkedList.to_list
+      ) |> to(eq [])
+    end
+
+    it "concats a linked list with an empty linked list" do
+      expect(
+        LinkedList.concat(LinkedList.create(1), :empty)
+        |> LinkedList.to_list
+      ) |> to(eq [1])
+    end
+
+    it "concats an empty linked list with a linked list" do
+      expect(
+        LinkedList.concat(:empty, LinkedList.create(5))
+        |> LinkedList.to_list
+      ) |> to(eq [5])
+    end
+
+    it "concats a linked list with a linked list" do
+      expect(
+        LinkedList.concat(LinkedList.create(1), LinkedList.create(2))
+        |> LinkedList.to_list
+      ) |> to(eq [1, 2])
+    end
+  end
+
+  context "sort" do
+    it "returns an empty linked list if given an empty linked list" do
+      expect(
+        []
+        |> LinkedList.from_list
+        |> LinkedList.sort
+        |> LinkedList.to_list
+      ) |> to(eq [])
+    end
+
+    it "sorts a single-node linked list" do
+      expect(
+        [1]
+        |> LinkedList.from_list
+        |> LinkedList.sort
+        |> LinkedList.to_list
+      ) |> to(eq [1])
+    end
+
+    it "sorts a large linked list" do
+      expect(
+        [5, 4, 3, 2, 1]
+        |> LinkedList.from_list
+        |> LinkedList.sort
+        |> LinkedList.to_list
+      ) |> to(eq [1, 2, 3, 4, 5])
+    end
+  end
+
+  context "reverse" do
+    it "returns an empty linked list if given an empty linked list" do
+      expect(
+        []
+        |> LinkedList.from_list
+        |> LinkedList.reverse
+        |> LinkedList.to_list
+      ) |> to(eq [])
+    end
+
+    it "reduces a single-node linked list if given a single-node linked" do
+      expect(
+        [1]
+        |> LinkedList.from_list
+        |> LinkedList.reverse
+        |> LinkedList.to_list
+      ) |> to(eq [1])
+    end
+
+    it "reverses a large linked list" do
+      expect(
+        [1, 2, 3, 4, 5]
+        |> LinkedList.from_list
+        |> LinkedList.reverse
+        |> LinkedList.to_list
+      ) |> to(eq [5, 4, 3, 2, 1])
+    end
+  end
+
+  context "every" do
+    it "returns false if given an empty linked list" do
+      expect(
+        []
+        |> LinkedList.from_list
+        |> LinkedList.every(fn x -> x < 5 end)
+      ) |> to(eq false)
+    end
+
+    it "checks a single-node linked list" do
+      expect(
+        [1]
+        |> LinkedList.from_list
+        |> LinkedList.every(fn x -> x < 5 end)
+      ) |> to(eq true)
+    end
+
+    it "checks a large linked list" do
+      expect(
+        [1, 2, 4, 8, 2]
+        |> LinkedList.from_list
+        |> LinkedList.every(fn x -> x < 5 end)
+      ) |> to(eq false)
+    end
+  end
+
+  context "some" do
+    it "returns false if given an empty linked list" do
+      expect(
+        []
+        |> LinkedList.from_list
+        |> LinkedList.some(fn x -> x < 5 end)
+      ) |> to(eq false)
+    end
+
+    it "checks a single-node linked list" do
+      expect(
+        [1]
+        |> LinkedList.from_list
+        |> LinkedList.some(fn x -> x < 5 end)
+      ) |> to(eq true)
+    end
+
+    it "checks a large linked list" do
+      expect(
+        [9, 8, 4, 6]
+        |> LinkedList.from_list
+        |> LinkedList.some(fn x -> x < 5 end)
+      ) |> to(eq true)
+    end
+
+    it "checks a large linked list" do
+      expect(
+        [9, 8, 7, 6]
+        |> LinkedList.from_list
+        |> LinkedList.some(fn x -> x < 5 end)
+      ) |> to(eq false)
+    end
+  end
+
+  context "eq" do
+    it "returns true if given empty linked lists" do
+      expect(LinkedList.eq(:empty, :empty)) |> to(eq true)
+    end
+
+    it "checks if a non-empty linked list is equal to an empty linked list" do
+      expect(
+        LinkedList.eq(LinkedList.create(1), :empty)
+      ) |> to(eq false)
+    end
+
+    it "checks if an empty linked list is equal to a non-empty linked list" do
+      expect(
+        LinkedList.eq(:empty, LinkedList.create(1))
+      ) |> to(eq false)
+    end
+
+    it "checks if identical linked lists are equal to each other" do
+      expect(
+        LinkedList.eq(LinkedList.create(1), LinkedList.create(1))
+      ) |> to(eq true)
+    end
+
+    it "checks if different linked lists are equal to each other" do
+      expect(
+        LinkedList.eq(LinkedList.create(1), LinkedList.create(2))
+      ) |> to(eq false)
+    end
+  end
+end
