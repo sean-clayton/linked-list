f95049656fe37aca6338126deb6448beb25274fc
Sean Clayton <sean@seanclayton.me>
2017-05-01 14:31:18 -0400

    Add coveralls support

--- mix.exs
+++ mix.exs
@@ -9,34 +9,30 @@ defmodule LinkedList.Mixfile do
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      test_coverage: [
+        test_task: "espec",
+        tool: ExCoveralls
+      ],
       preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
         espec: :test,
       ]
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:credo, "~> 0.7", only: [:dev, :test]},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:espec, "~> 1.3.4", only: [:dev, :test]},
+      {:excoveralls, "~> 0.6", only: :test},
     ]
   end
 end
