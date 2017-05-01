5579492331d41ee4f7ad7c80fe5d9fa1a1a94884
Sean Clayton <sean@seanclayton.me>
2017-05-01 12:38:35 -0400

    Add more reduce implementations

--- lib/enumerable.ex
+++ lib/enumerable.ex
@@ -7,7 +7,8 @@ defimpl Enumerable, for: LinkedList do
     {:ok, LinkedList.some(node, fn x -> x == value end)}
   end
 
-  def reduce(node, initial_value, fun) do
-    {:ok, LinkedList.reduce(node, initial_value, fun)}
-  end
+  def reduce(_, {:halt, acc}, _fun), do: {:halted, acc}
+  def reduce(node, {:suspend, acc}, fun), do: {:suspended, acc, &reduce(node, &1, fun)}
+  def reduce(%LinkedList{value: nil, next: nil}, {:cont, acc}, _fun), do: {:done, acc}
+  def reduce(node, initial_value, fun), do: LinkedList.reduce(node, initial_value, fun)
 end
